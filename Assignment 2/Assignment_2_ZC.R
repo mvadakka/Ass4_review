3d805f339b1ca1bafcd826b5d01eea6a65f87176
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-13 19:53:53 -0400

    Cleaned up the comments, added some other comments for readability

--- Assignment 2/Assignment_2_ZC.R
+++ Assignment 2/Assignment_2_ZC.R
@@ -1,26 +1,24 @@
-#'Assignment 2 Instructions:
-#'Prompt the user to enter a three digit positive number
+#' BTC1855 - Assignment 2, Zachery Chan
+#' R version: Version 2024.04.1+748 (2024.04.1+748)
 
-#'Check if the user input is numeric. If not, print an error message and quit.
+#Taking user input using readline(), didnt use scan() as scan() 
+#   specifies file path while readline() just goes through console commands
+user_input <- readline("Let's play a game, think of 3, single-digit, positive numbers and put'em together. Enter your number here please: ")
 
-#'Check if the number is narcissistic. A narcissistic number, or an Armstrong number,
-#' - is a number that is equal to the sum of the cubes of its own digits. 153, 370, 371, 407 
-#' - are three digit Armstrong numbers.
-
-#'Display the result with an appropriate message, e.g. “127 is not an Armstrong number” 
-#'- or “370 is a narcissistic number” or any additional fun text you may want to add.
-#'- This is a test commit for assignment 2 for BTC1855
-
-
-user_input <- readline("Enter your number here please: ")
+#----------------------------------------
 
 #Please input your number here now 
 
-if (is.na(c(as.numeric(user_input))) == T) {
-  stop("THAT IS NOT A NUMBER, SILLY")
+#---------------------------------------
+
+#Using if() to see if input is numeric, if it is a character, returns NA and stops
+if (is.numeric(c(as.numeric(user_input))) == T) {
+  stop ("THAT IS NOT A NUMBER, SILLY! Run it again and please put in a number, I will be watching...")
+  quit ()
 }
 
-#splitting user input into 3 numbers (uses character string as strsplit() uses chracters not strings)
+#'Splitting user input into 3 numbers (uses character string as 
+#'   strsplit() uses characters not strings)
 split_input <- strsplit(user_input, "")
 
 input_string <- split_input[[1]]
@@ -31,14 +29,17 @@ second_number <- as.numeric(input_string[2])
 
 third_number <- as.numeric(input_string[3])
 
+#'Calculating if input is an Armstrong number or not
 armstrong <- first_number^3 + second_number^3 + third_number^3
 
+#' Now converting user input to numeric (didn't need to earlier) to compare
 user_numeric <- as.numeric(user_input) 
 
 is_armstrong <- user_numeric == armstrong 
 
+#' Using conditional to determine which message is given
 if (is_armstrong == T) {
-  print (paste(user_input, "is a narcissistic number, you self-lover you. Wanna try again?"))
+  print (paste(user_input, "is a narcissistic number, you self-lover you :)"))
  } else if (is_armstrong == F) {
-    print (paste(user_input, "is a not a narcissistic number. "))
+    print (paste(user_input, "is a NOT a narcissistic number. Maybe we should talk about your self image?"))
   }
