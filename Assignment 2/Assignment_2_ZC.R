e6299df612ddf128d118d76364616232915ba0d8
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-14 18:07:38 -0400

    merged the else if into one using logical operators

--- Assignment 2/Assignment_2_ZC.R
+++ Assignment 2/Assignment_2_ZC.R
@@ -2,28 +2,27 @@
 #' R version: Version 2024.04.2+764 (2024.04.2+764)
 #' Code version date: June 14th
 
-#Taking user input using readline(), didnt use scan() as scan() 
-#   specifies file path while readline() just goes through console commands
+# Taking user input using readline(), didnt use scan() as scan() 
+# specifies file path while readline() just goes through console commands
 user_input <- readline(prompt = "Hey, play a game with me? Think of 3, single-digit, positive numbers and put'em together. Enter your number here please: ")
 
 #----------------------------------------
 
-#Please input your number here now 
+# Please input your number here now 
 
 #---------------------------------------
 
 #' Now converting user input to numeric to compare
 user_numeric <- as.numeric(user_input)
 
-#Using if() to see if input is numeric, if it is a character, stops
-if (is.na(as.numeric(user_input)) == T) {
-  stop ("ERROR! ERROR! THAT IS NOT A NUMBER, SILLY! \n See these NA warnings? I was forced to do this :( \n Run it again and please put in a number, I will be watching...")
-} else if (user_numeric <= 0) {
-  stop ("ERROR! ERROR! THAT IS A NEGATIVE NUMBER, SILLY! \n See these NA warnings? I was forced to do this :( \n Please follow the instructions this time. \n Run it again and please put in a number, I will be watching...")
-}
+# Using if() and logical statements to combine conditions 
+# Seeing if input is not numeric OR if input is negative, if so, it stops
+if (is.na(as.numeric(user_input)) == T | user_numeric <= 0) {
+  stop ("ERROR! ERROR! THAT DOES NOT FOLLOW THE INSTRUCTIONS, SILLY! \n See these NA warnings? I was forced to do this :( \n Run it again and please put in a number, I will be watching...")
+} 
 
-#'Splitting user input into 3 numbers (uses character string as 
-#'   strsplit() uses characters not strings)
+#' Splitting user input into 3 numbers (uses character string as 
+#' strsplit() uses characters not strings)
 split_input <- strsplit(user_input, "")
 
 input_string <- split_input[[1]]
@@ -49,4 +48,3 @@ if (user_numeric == armstrong ) {
  } else if (is_armstrong == F) {
     print (paste(user_input, "is a NOT a narcissistic number. Maybe we should talk about your self image?"))
   }
-
