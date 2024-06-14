c9483f55a8abf7e0f180b3af884bf00ad3f0b9ae
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-14 18:18:18 -0400

    Edited code date & comments

--- Assignment 2/Assignment_2_ZC.R
+++ Assignment 2/Assignment_2_ZC.R
@@ -1,6 +1,6 @@
 #' BTC1855 - Assignment 2, Zachery Chan
 #' R version: Version 2024.04.2+764 (2024.04.2+764)
-#' Code version date: June 14th
+#' Code dated to: June 14th
 
 # Taking user input using readline(), didnt use scan() as scan() 
 # specifies file path while readline() just goes through console commands
@@ -16,7 +16,7 @@ user_input <- readline(prompt = "Hey, play a game with me? Think of 3, single-di
 user_numeric <- as.numeric(user_input)
 
 
-# Using if() and logical statements to combine conditions 
+# Using if() and logical OR statement to combine conditions 
 # Seeing if input is not numeric OR if input is negative, if so, it stops
 if (is.na(as.numeric(user_input)) == T | user_numeric <= 0) {
   stop ("ERROR! ERROR! THAT DOES NOT FOLLOW THE INSTRUCTIONS, SILLY! \n See any NA warnings? I was forced to do this :( \n Run it again and please put in a number, I will be watching...")
@@ -43,7 +43,8 @@ third_number <- as.numeric(input_string[3])
 #'Calculating if input is an Armstrong number or not
 armstrong <- first_number^3 + second_number^3 + third_number^3
 
-#' Using conditional to determine which message is given
+#' Using conditional to determine if the number is an armstrong number and 
+#' which message is given
 #' Using else if here because, honestly, I just wanted to... 
 if (user_numeric == armstrong ) {
   print (paste(user_input, "is a narcissistic number, you self-lover you :). Now either go look at your reflection in a lake ;)"))
