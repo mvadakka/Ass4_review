0cba54a972a84f7eb6ea2306c396370b88ef3918
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-14 15:49:52 -0400

    changed the if numerical evaluator for user input again. This evaluates in one line and saves teh conversion of suer input to later only if the input is numerical (minimizing error messages)

--- Assignment 2/Assignment_2_ZC.R
+++ Assignment 2/Assignment_2_ZC.R
@@ -12,8 +12,8 @@ user_input <- readline("Hey, play a game with me? Think of 3, single-digit, posi
 #---------------------------------------
 
 #Using if() to see if input is numeric, if it is a character, returns NA and stops
-if (is.numeric(c(as.numeric(user_input))) == T) {
-  stop ("ERROR! ERROR! THAT IS NOT A NUMBER, SILLY! Run it again and please put in a number, I will be watching...")
+if (is.na(as.numeric(user_input)) == T) {
+  stop ("ERROR! ERROR! THAT IS NOT A NUMBER, SILLY! \n Run it again and please put in a number, I will be watching...")
 }
 
 #'Splitting user input into 3 numbers (uses character string as 
@@ -31,14 +31,14 @@ third_number <- as.numeric(input_string[3])
 #'Calculating if input is an Armstrong number or not
 armstrong <- first_number^3 + second_number^3 + third_number^3
 
-#' Now converting user input to numeric (didn't need to earlier) to compare
+#' Now converting user input to numeric to compare
 user_numeric <- as.numeric(user_input) 
 
 is_armstrong <- user_numeric == armstrong 
 
 #' Using conditional to determine which message is given
 if (is_armstrong == T) {
-  print (paste(user_input, "is a narcissistic number, you self-lover you :)"))
+  print (paste(user_input, "is a narcissistic number, you self-lover you :). Now either go look at your reflection in a lake ;)"))
  } else if (is_armstrong == F) {
     print (paste(user_input, "is a NOT a narcissistic number. Maybe we should talk about your self image?"))
   }
