bd83ce95ec0c8191372940330e92e75ee0bcf14a
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-27 15:11:59 -0400

    Added a whole bunch, added read file, added some prompts, set luves to 5, wrote code to identify index of matchign letter in word

--- Assignment 3/Assignment_3_ZC.R
+++ Assignment 3/Assignment_3_ZC.R
@@ -6,4 +6,34 @@ word <- sample(word_list,1)
 
 word_length <- nchar(word)
 
-print
+print(paste("The chosen word is", word_length, "characters long."))
+
+print("You will get 5 lives in this game of hangman")
+
+lives <- 5
+
+user_input <- readline(prompt = "enter a letter: ")
+
+numeric_check <- ifelse(is.na(as.numeric(user_input))==T, 1, 0)
+if (numeric_check == 0) {
+  stop ("ERROR! ERROR! THAT DOES NOT FOLLOW THE INSTRUCTIONS, SILLY! \n That is not a letter, silly! \n See any NA warnings? I was forced to do this :( \n Run it again and please put in a letter, I will be watching...")
+} 
+
+blank_string <- paste(rep("(_)", word_length), collapse = "")
+
+while (lives <= 1){
+  word_list <- strsplit(word, "")
+  split_word <- test[[1]]
+  
+  test <- c()
+  
+  for (i in seq_along(split_word)) {
+    if (identical(split_word, user_input)){ 
+      #concatenating blankl vector, so rresult is only a single element vector with the matching index
+      index_match <- c(test, i)
+    }
+}
+
+
+
+
