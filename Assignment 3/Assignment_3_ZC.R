438f26ae11a8b3af14cd497f16f1afc21b396105
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-27 17:23:19 -0400

    changed approach to use a vector to display word rather than string, much easier to work with and subsect to display

--- Assignment 3/Assignment_3_ZC.R
+++ Assignment 3/Assignment_3_ZC.R
@@ -19,20 +19,37 @@ if (numeric_check == 0) {
   stop ("ERROR! ERROR! THAT DOES NOT FOLLOW THE INSTRUCTIONS, SILLY! \n That is not a letter, silly! \n See any NA warnings? I was forced to do this :( \n Run it again and please put in a letter, I will be watching...")
 } 
 
-blank_string <- paste(rep("(_)", word_length), collapse = "")
+blank_vector <- c(rep("_", word_length))
 
-while (lives <= 1){
-  word_list <- strsplit(word, "")
-  split_word <- test[[1]]
-  
+word_list <- strsplit(word, "")
+split_word <- word_list[[1]]
+test <- c()
+
+for (i in seq_along(split_word)) {
+  if (identical(split_word[i], user_input)){ 
+    #concatenating blankl vector, so rresult is only a single element vector with the matching index
+    index_match <- c(test, i)
+    blank_vector[index_match] <- user_input
+    
+    print(blank_vector)
+  }
+}
+
+
+
+if (str_detect(word, user_input) == T) {
   test <- c()
-  
   for (i in seq_along(split_word)) {
     if (identical(split_word, user_input)){ 
       #concatenating blankl vector, so rresult is only a single element vector with the matching index
       index_match <- c(test, i)
+      
+      blank_vector[index_match] <- user_input
     }
+  }
 }
+
+
 
 
 
