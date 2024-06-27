68bf437983615a30bdd87c114d6a5a1f9a0bbee5
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-27 11:26:48 -0400

    Created plan for assignment 3

---
+++ Assignment 3/Assignment_3_Plan.R
@@ -0,0 +1,15 @@
+#' Plan for Assignment 3 
+#' 
+#' make list -> 10 words? 
+#' read list 
+#' 
+#' use sample to get randomw word
+#' 
+#' inform user of length
+#' 
+#' set rules: 
+#' lives --> 5? 
+#' set lives to 5 
+#' 
+#' Set loop? 
+#' use if to check if lives is less 
