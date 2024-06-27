07818cdda33a7d4d2f33b1820d3db13b64abb596
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-06-27 11:14:31 -0400

    Changed rtf filr to txt file, started coding, added in first parts of reading file and sampling

--- Assignment 3/Assignment_3_ZC.R
+++ Assignment 3/Assignment_3_ZC.R
@@ -1,1 +1,9 @@
-Testing for save 
+file_path <- "/Users/zachery/BTC1855/Assignment 3/ListofWords.txt"
+
+word_list <- readLines(file_path)
+
+word <- sample(word_list,1)
+
+word_length <- nchar(word)
+
+print
