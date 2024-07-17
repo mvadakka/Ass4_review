d52678f96ddc4d8827ca3ba72568d5777ec8db89
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-07-17 18:29:49 -0400

    fixed most structural and data content problems

--- Assignment_4/Assignment_4.R
+++ Assignment_4/Assignment_4.R
@@ -1,32 +1,100 @@
 file_path <- c("/Users/zachery/BTC1855/Assignment_4/ufo_subset.csv")
 data <- read.csv(file_path)
 
-summary()
+library(lubridate)
+library(stringr)
+library(dplyr)
 
-
+# Creating new data frame 
 data1 <- data
-data1$state_alt <- factor(data1$state)
 
-data1$state_alt
-#address missing 
+#Renaming datetime to sighting time
+data1 <- data1 %>%
+  select(sighttime = datetime,
+         city,
+         state,
+         country, 
+         shape, 
+         duration.seconds, 
+         duration.hours.min, 
+         comments,
+         date_posted,
+         latitude, 
+         longitude)
 
-data1$state_alt[data1$state_alt == ""] <- NA
-#remove empry bin 
+# Factoring state, storing new cariable
+data1$state_fctr <- factor(data1$state)
 
-data1$country_alt[data1$country_alt == ""] <- NA
-#remove empry bin 
+#Addressing missing, redefining as NA 
+data1$state_fctr[data1$state_fctr == ""] <- NA
 
-#address missing 
+#Redefining levels of state factor to remove "" bin as it is empty 
+state_lvls <- levels(data1$state_fctr)
+state_lvls <- state_lvls[state_lvls != ""]
+
+#Redefining state factor with new levels 
+data1$state_fctr <- factor(data1$state, levels = state_lvls)
+
+# Factoring country, storing new variable
+data1$country_fctr <- factor(data1$country)
+
+#Addressing missing, redefining as NA 
+data1$country_fctr[data1$country_fctr == ""] <- NA
+
+#Redefining levels of state factor to remove "" bin as it is empty 
+country_lvls <- levels(data1$country_fctr)
+country_lvls <- country_lvls[country_lvls != ""]
+
+#Redefining state factor with new levels 
+data1$country_fctr <- factor(data1$country, levels = country_lvls)
+
+#Converting date to a date type 
+data1$date_posted_alt <- strptime(data1$date_posted, format = "%d-%m-%Y")
+#data1$date_posted_alt <- dmy(data1$date_posted)
 
 
+#------------------------
+#data1$duration.hours.min[str_detect(duration.hours.min, "sec")] <- c(">1 min")
+#------------------------
+#Converting sighttime into Posix 
+data1$sighttime_alt <- ymd_hm(data1$sighttime)
+#data1$sighttime_alt <- strptime(data1$sighttime, format = "%Y-%m-%d %H:%M", tz = "UTC")
 
-#' strucutrals iisues
-#' convert dates using posex
-#' convert states ans countries into factors 
-#' 
-#' 
-#' data issues (identify) 
-#' check for NAs 
-#' check for missing (blanks)
-#' check for extreme values 
-#' check for duplicates 
+# Factoring state, storing new cariable
+data1$shape_fctr <- factor(data1$shape)
+
+#Addressing missing, redefining as NA 
+data1$shape_fctr[data1$shape_fctr == ""] <- NA
+
+#Redefining levels of state factor to remove "" bin as it is empty 
+shape_lvls <- levels(data1$shape_fctr)
+shape_lvls <- shape_lvls[shape_lvls != ""]
+
+#Redefining state factor with new levels 
+data1$shape_fctr <- factor(data1$shape, levels = shape_lvls)
+
+#Using EST as tz but shouldnt matter as measuring duration
+data1$duration.seconds_alt <- as.POSIXct(duration.seconds, tz = "UTC")
+
+#Setting missing comments as NA 
+data1$comments_alt <- data1$comments
+data1$comments_alt[data1$comments_alt == ""] <- NA
+
+#Making new dataframe with corrected structural issues 
+data2 <- data.frame(data1$sighttime_alt, data1$city, data1$state_fctr, data1$country_fctr, 
+                    data1$shape_fctr, data1$duration.seconds_alt, data1$duration.hours.min, 
+                    data1$comments_alt, data1$date_posted_alt, data1$latitude, data1$longitude)
+
+colnames(data2) <- c("sighttime", "city", "state", "country",
+                     "shape", "duration.seconds", "duration.hours.min", 
+                     "comments", "date_posted", "latitude", "longitude")
+
+# Checking for any blanks 
+column_blnk <- apply(data2, 2, function(col) any(col == ""))
+#Checking for NAs
+column_na <- apply(data2, 2,  function(col) any(is.na(col)))
+# Checking for exact duplicates 
+anyDuplicated(data2)
+
+#' check for extreme values??? 
+
